e48841c4bd760003455a53ea30d841090ef295e2
Sidney <sidneyfranciscodev@gmail.com>
2026-01-23 19:46:02 +0200

    create and style navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,3 +1,12 @@
 <template>
-  <nav></nav>
+  <nav id="navbar">
+    <div class="content">
+      <h1><router-link to="/">SF</router-link></h1>
+      <ul>
+        <li><router-link to="/projects">Projects</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </div>
+  </nav>
 </template>
