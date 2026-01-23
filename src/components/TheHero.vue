d039f627362b821164c1b54cea56f36cfc570d26
Sidney <sidneyfranciscodev@gmail.com>
2026-01-23 20:31:04 +0200

    create and style the hero section

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,3 +1,19 @@
 <template>
-  <section></section>
+  <section id="hero">
+    <div class="badge">
+      <div class="content">
+        <span>Open to opportunities</span>
+      </div>
+    </div>
+
+    <h1 class="title">
+        <span class="gradient">Software Engineer</span>
+        <br>
+        <span class="primary">Focused on Clean Architecture, Security, and AI-driven products</span>
+    </h1>
+
+    <p class="subtitle">
+      I design and build production-ready applications — from backend architecture to frontend delivery — with security, performance, and maintainability as first-class concerns.
+    </p>
+  </section>
 </template>
